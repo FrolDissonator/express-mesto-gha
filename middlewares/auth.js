eaddf17b48fee1831505ae08d42c632329f73038
Sergey Frolov <froldissonator@yandex.ru>
2023-09-17 23:48:42 +0300

    Change user controller

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,24 +1,20 @@
 const jwt = require('jsonwebtoken');
 const ApiError = require('../errors/ApiError');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
-const secret = NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key';
-
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(ApiError.unauthorized('Необходима авторизация'));
+    throw ApiError.unauthorized('Необходима авторизация');
   }
 
   const token = authorization.replace('Bearer ', '');
-
   let payload;
 
   try {
-    payload = jwt.verify(token, secret);
+    payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return next(ApiError.unauthorized('Некорректный токен'));
+    throw ApiError.unauthorized('Некорректный токен');
   }
 
   req.user = payload;
